18f354b89fecc3943f8ce535eb84017229140d55
rekha kumawat <rekhasumanku@gmail.com>
2026-03-16 22:51:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+function toggleMenu(){
+
+const nav = document.querySelector(".nav-links");
+
+if(nav.style.display === "flex"){
+nav.style.display = "none";
+}
+else{
+nav.style.display = "flex";
+}
+
+}
